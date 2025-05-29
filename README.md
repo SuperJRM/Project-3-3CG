22c1dbba738a7c80e429d69afaa06bb3042480d2
SuperJRM <163477766+SuperJRM@users.noreply.github.com>
2025-05-28 23:20:49 -0700

    Repo Setup and Extension

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Project-3-3CG
+-- Jason Rangel-Martinez
+-- CMPM 121 - 3CG
+-- 5/28/25
 
+I'm still working on project 3. Thank you for your patience.
---
+++ main.lua
@@ -0,0 +1,18 @@
+-- Jason Rangle-Martinez
+-- CMPM 121 - 3CG Project
+-- 5/28/25
+io.stdout:setvbuf("no")
+
+function love.load()
+  love.window.setMode(960,640)
+  love.window.setTitle("THE 3CG")
+  love.window.setMode(960, 640)
+end
+
+function love.draw()
+
+end
+
+function love.update()
+
+end
